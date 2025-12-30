e89abe6a972137a2fcdaa2adc7d72f6b35e4172f
skyyy <aakashprajapati2457@gmail.com>
2025-12-30 12:37:39 +0530

    InternRepo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import React from 'react'
+import { Routes , Route } from 'react-router-dom'
+import Login from './component/Login'
+function App() {
+
+
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
